9d4179b9d26e1abbcfa606d03dd4646f06ced7ce
chubbymario <chubbymario101@gmail.com>
2024-06-11 16:55:24 +0800

    Add Main Page

---
+++ Streamlit/Pages/.ipynb_checkpoints/Page_3-checkpoint.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello!", page_icon="🔥")
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
---
+++ Streamlit/Pages/Page_2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello!", page_icon="🔥")
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
